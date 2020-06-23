5fb0619e7d58aeb4fd19b801fbe9235a9ba70a7a
John Mercer <presci3nce@gmail.com>
2020-06-23 07:33:36 -0400

    updated the api url in frontend env variables to the exposed url of reverse proxy

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a14c09477371844998d018f4733fb3b8-681253174.us-east-2.elb.amazonaws.com/api/v0'
 };
 
 /*
